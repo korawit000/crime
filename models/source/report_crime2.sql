fc53fd8d6c2bfad9463dcab03841af7600fbeca9
mikeKP <mikekorawit@hotmail.com>
2025-04-23 21:40:12 +0700

    test

--- models/source/report_crime2.sql
+++ models/source/report_crime2.sql
@@ -1,11 +1,36 @@
 {{ config(materialized='incremental', unique_key='dr_no') }}
 
 with source as (
-SELECT "LOCATION", "Weapon Used Cd", "Status Desc", "DATE OCC", "Weapon Desc", "Premis Desc", "_ab_source_file_url", area,
- "TIME OCC", "Premis Cd", "Rpt Dist No", "Part 1-2", "AREA NAME", "Vict Sex", status, "_ab_source_file_last_modified"
- , lon, "Crm Cd 1", "Crm Cd 2", "Crm Cd", "Crm Cd 3", "Crm Cd 4", "Crm Cd Desc", "Vict Descent"
- , mocodes, "Date Rptd", dr_no, "Vict Age", "Cross Street", "_ab_additional_properties", lat,
-  "_airbyte_ab_id", "_airbyte_emitted_at", "_airbyte_normalized_at", "_airbyte_crime_data_hashid"
+SELECT 
+  dr_no
+  ,date_rptd
+  ,date_occ
+  ,time_occ
+  ,area
+  ,area_name
+  ,rpt_dist_no
+  ,part_1_2
+  ,crm_cd
+  ,crm_cd_desc
+  ,mocodes
+  ,vict_age
+  ,vict_age AS vict_age_range
+  ,vict_sex
+  ,vict_descent
+  ,premis_cd
+  ,premis_desc
+  ,weapon_used_cd
+  ,weapon_desc
+  ,"status"
+  ,status_desc
+  ,crm_cd_1
+  ,crm_cd_2
+  ,crm_cd_3
+  ,crm_cd_4
+  ,"location"
+  ,cross_street
+  ,lat
+  ,lon
 FROM analytic.crime_data
 )
 
