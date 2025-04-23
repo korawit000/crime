02e8ba46a8ec12e1cbffec393baeda4614f26866
mikeKP <mikekorawit@hotmail.com>
2025-04-23 19:39:13 +0700

    test

---
+++ models/stage/stg_user_activity_per_country.sql
@@ -0,0 +1,64 @@
+
+-- {{ config(materialized='view') }}
+
+-- WITH combined_activities AS (
+--   SELECT
+--     country,
+--     'listen_event' AS event_type,
+--     ts,
+--     userid
+--   FROM {{ ref('src_listen_events') }}
+--   WHERE country IS NOT NULL
+--   UNION ALL
+--   SELECT
+--     'Unknown' AS country, 
+--     ts,
+--     userid
+--   FROM {{ ref('src_auth_events') }}
+--   UNION ALL
+--   SELECT
+--     'Unknown' AS country, 
+--     'page_view_event' AS event_type,
+--     ts,
+--     userid
+--   FROM {{ ref('src_page_view_events') }}
+-- )
+
+-- SELECT
+--   country,
+--   COUNT(*) AS activity_count
+-- FROM combined_activities
+-- GROUP BY country
+
+
+{{ config(materialized='view') }}
+
+WITH combined_activities AS (
+  SELECT
+    country,
+    'listen_event' AS event_type,
+    ts,
+    userid
+  FROM {{ ref('src_listen_events') }}
+  WHERE country IS NOT NULL
+  UNION ALL
+  SELECT
+    'Unknown' AS country, 
+    'auth_event' AS event_type,  -- เพิ่ม event_type ที่ขาด
+    ts,
+    userid
+  FROM {{ ref('src_auth_events') }}
+  UNION ALL
+  SELECT
+    'Unknown' AS country, 
+    'page_view_event' AS event_type,  -- เพิ่ม event_type
+    ts,
+    userid
+  FROM {{ ref('src_page_view_events') }}
+)
+
+SELECT
+  country,
+  COUNT(*) AS activity_count
+FROM combined_activities
+GROUP BY country
